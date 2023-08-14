aa518f3dd46e7bdcb55c262570a7e6abedaf7c17
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-08-14 23:12:40 +0530

    add: tab

--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -1,21 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useTheme } from "../../Context/ThemeContext";
 import "./Header.css";
 
 const Header = () => {
   const { isDarkTheme, toggleTheme } = useTheme();
   return (
-    <header>
-      <button
-        className={`mode ${isDarkTheme ? "dark" : "light"}-mode`}
-        variant="contained"
-        onClick={() => toggleTheme(!isDarkTheme)}
-        aria-label="Theme Toggle Button"
-      >
-        <span
-          className={`circle ${isDarkTheme ? "dark" : "light"}-circle`}
-        ></span>
-      </button>
+    <header
+      className={`px-2 py-4 text-xl ${isDarkTheme ? "dark" : "light"}-header`}
+    >
+      <div className="flex justify-between items-center">
+        <h1>
+          <Link to="/">Task Sync</Link>
+        </h1>
+        <button
+          className={`mode ${isDarkTheme ? "dark" : "light"}-mode`}
+          variant="contained"
+          onClick={() => toggleTheme(!isDarkTheme)}
+          aria-label="Theme Toggle Button"
+        >
+          <span
+            className={`circle ${isDarkTheme ? "dark" : "light"}-circle`}
+          ></span>
+        </button>
+      </div>
     </header>
   );
 };
--- src/Layout/PageWrapper/index.jsx
+++ src/Layout/PageWrapper/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header";
 import Main from "../Main";
+import Tab from "../Tab";
 
 const PageWrapper = ({ children }) => {
   return (
     <>
       <Header />
       <Main mainContent={children} />
+      <Tab />
     </>
   );
 };

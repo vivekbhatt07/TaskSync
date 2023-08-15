62c06d57b9b81f58aa930d2547135e3b380ae3bc
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-08-15 18:48:39 +0530

    add: chart

---
+++ src/Components/Diagrams/BarGraph/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BarGraph = () => {
+  return <div></div>;
+};
+
+export default BarGraph;
--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,5 +1,13 @@
 import { OutlinedAction } from "./Actions";
 import { PrimaryCard, SecondaryCard } from "./Cards";
+import { BarGraph, PieChart } from "./Diagrams";
 import Filter from "./Filter";
 
-export { OutlinedAction, PrimaryCard, SecondaryCard, Filter };
+export {
+  OutlinedAction,
+  PrimaryCard,
+  SecondaryCard,
+  Filter,
+  BarGraph,
+  PieChart,
+};
